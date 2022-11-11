974c889e1dea353876926a3f117e7e8fb39bfae9
Joaquin Espiñeira <43030598@comunidadort.edu.ar>
2022-11-10 23:01:52 -0300

    Tercer Commit

--- CabañasProyecto/Controllers/ReservaController.cs
+++ CabañasProyecto/Controllers/ReservaController.cs
@@ -1,25 +1,57 @@
 ﻿using CabañasProyecto.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 
 namespace CabañasProyecto.Controllers
 {
+
     public class ReservaController : Controller
     {
+        // Averiguar como poner un menu desplegable
         [HttpGet]
         public IActionResult Index()
         {
-            List<Reserva> reservas = new();
+            List<Cabania> reservas = new();
             using (CabaniasContext c = new())
             {
-                reservas = c.Reservas.ToList();
+                //Linq
+                //List<Cabania> reservas2 = (from ca in c.Cabanias where ca.Estado == true select ca).ToList();
+                reservas = c.Cabanias.Where(pepe => pepe.Estado).ToList(); // sentencia lamba
             }
             return View(reservas);
         }
         [HttpGet]
         public IActionResult Create()
         {
+            List<Cabania> reservas = new();
+            using (CabaniasContext c = new())
+            {
+                reservas = c.Cabanias.Where(c => !c.Estado).ToList();
+                ViewBag.Cabanias = reservas;
+            }
             return View();
         }
-        
+       
+        [HttpPost]
+        public IActionResult Create(Reserva reserva)
+        {
+            Cabania? cabania = null;
+            using (CabaniasContext c = new CabaniasContext())
+            {
+                reserva.IdCabania = reserva.Id;
+                reserva.Id = 0;
+                c.Reservas.Add(reserva);
+                cabania = c.Cabanias.Find(reserva.IdCabania);
+                if (cabania != null)
+                {
+                    cabania.Estado = true;
+                    c.SaveChanges();
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
+ 
+
---
+++ CabañasProyecto/Migrations/20221110193549_inicial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CabañasProyecto.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cabanias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cabanias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DNI = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdCabania = table.Column<int>(type: "int", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cabanias");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Reservas");
+        }
+    }
+}
